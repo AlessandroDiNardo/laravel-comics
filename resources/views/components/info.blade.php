002a3344478d38d715194405476fa7f67505bd04
AlessandroDiNardo <alexdna003@gmail.com>
2023-01-31 00:47:08 +0100

    create info section

---
+++ resources/views/components/info.blade.php
@@ -0,0 +1,27 @@
+<!-- info main -->
+<section class="info_main">
+    <div class="container">
+        <div class="info_container">
+            <div class="info_card">
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="">
+                <div>DIGITAL COMICS</div>
+            </div>
+            <div class="info_card">
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="">
+                <div>DC MERCHANDISE</div>
+            </div>
+            <div class="info_card">
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="">
+                <div>SUBSCRIPTIONS</div>
+            </div>
+            <div class="info_card">
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="">
+                <div>COMIC SHOP LOCATOR</div>
+            </div>
+            <div class="info_card">
+                <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="">
+                <div>DCPOWER VISA</div>
+            </div>
+        </div>
+    </div>
+</section>
