bffabd67a7645e742bbdcf0fc725ba4ff2ccbb54
AlessandroDiNardo <alexdna003@gmail.com>
2023-01-30 20:10:48 +0100

    create lower main section

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -3,8 +3,122 @@
     <!-- <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="DC Jumbo"> -->
 </section>
 <!-- lower main -->
-<section>
+<section class="lower_main">
     <div class="container">
+        <h1>CURRENT SERIES</h1>
 
+        <div class="cards_container">
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>ACTION COMICS</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>AMERICAN VAMPIRE 1976</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>AQUAMAN</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>BATGIRL</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>BATMAN</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>BATMAN BEYOND</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>BATMAN/SUPERMAN</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>BATMAN/SUPERMAN ANNUAL</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>BATMAN: THE JOKER WAR ZONE</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>BATMAN: THREE JOKERS</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>BATMAN: WHITE KNIGHT PRESENTS: HARLEY QUEEN</div>
+                </div>
+            </div>
+
+            <div class="cards" >
+                <div class="cards-img">
+                    <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq" alt="DC Jumbo">
+                </div>
+                <div class="cards-text">
+                    <div>CATWOMAN</div>
+                </div>
+            </div>
+
+            <button>
+                <a href="">LOAD MORE</a>
+            </button>
+        </div>
     </div>
 </section>
